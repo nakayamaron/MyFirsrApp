deacc710a376c626c1a3101c1b1e50d867826215
Ryo Nakayama <nakayamaron@gmail.com>
2020-07-01 23:32:05 +0900

    途中セーブ

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products: number[] = [1,2,3,4];
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
