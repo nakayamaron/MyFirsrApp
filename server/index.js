4759c87b2a8071c4e6a044cc75e95d9316a7215e
Ryo Nakayama <nakayamaron@gmail.com>
2020-07-04 13:37:38 +0900

    Heroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,23 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require("./config/dev");
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config/");
 const FakeDb = require("./fake-db");
-const productRoutes = require('./routes/products')
+const productRoutes = require("./routes/products");
+const path = require("path");
 
-mongoose.connect(config.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  const fakeDb = new FakeDb()
-  fakeDb.initDb();
-})
+mongoose
+  .connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    if (process.env.NODE_ENV !== "production") {
+      const fakeDb = new FakeDb();
+      // fakeDb.initDb();
+    }
+  });
 
 const app = express();
 
-app.use('/api/v1/products', productRoutes);
+app.use("/api/v1/products", productRoutes);
 
-const PORT = process.env.PORT || '3001';
+if (process.env.NODE_ENV === "production") {
+  const appPath = path.join(__dirname, "..", "dist", "my-first-app");
+  app.use(express.static(appPath));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(appPath, "index.html"));
+  });
+}
 
-app.listen('3001', function() {
-  console.log('I am running')
-})
+const PORT = process.env.PORT || "3001";
+
+app.listen("3001", function () {
+  console.log("I am running");
+});
